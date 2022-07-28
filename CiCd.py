090d09beec80808a15d217f1684309f95d9ffa1d
benba <benbar101@gmail.com>
2022-07-28 16:28:02 +0300

    work kusemek

--- CiCd.py
+++ CiCd.py
@@ -1,10 +1,26 @@
 from docx2pdf import convert
-import os
-DocPath = r"C:\Users\benba\Ben Bar\CV - Ben Bar.docx"
-PdfPath = r"C:\Users\benba\Ben Bar\CV - Ben Bar.pdf"
-print("Converting the word file to pdf..")
-convert(DocPath)
-print("pushing both docs to the repo..")
-commit = input("Please type the commit: ")
-os.system("powershell git add .")
-os.system("powershell git commit -m " + commit)
+from git import Repo
+def main():
+    doc_convert()
+    git_push()
+
+def git_push():
+    PATH_OF_GIT_REPO = r'C:\Users\benba\Ben Bar\.git'
+    commit = input("Please type the commit: ")
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(update=True)
+        repo.index.commit(commit)
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print("Somthing went wrong while pushing the files")
+
+def doc_convert():
+    PATH_OF_WORD = r"C:\Users\benba\Ben Bar\CV - Ben Bar.docx"
+    print("Converting the word file to pdf..")
+    convert(PATH_OF_WORD)
+
+
+if __name__ == "__main__":
+    main()
